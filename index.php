84360c2b6266f7ca70740829afe79242fd7e7dd2
Yusron <57038230+Yrriz123@users.noreply.github.com>
2026-04-24 08:29:33 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+// API URL untuk mendapatkan data jumlah kejadian bencana berdasarkan jenisnya di Jawa Timur
+$apiUrl = 'https://opendata.jatimprov.go.id/api/cleaned-bigdata/dinas_komunikasi_dan_informatika_provinsi_jawa_timur/jumlah_pelayanan_jaringan_internet_instansilembaga?sort=id%3Aasc&page=1&per_page=10&where=%7B%22periode_update%22%3A%5B%222025-Q4%22%5D%7D&where_or=%7B%7D'; // Change this to your API URL
+
+//membuat header untuk menghidari 403
+$context = stream_context_create([
+    'http' => [
+        'method' => 'GET',
+        'header' => [
+            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+            'Accept: application/json',
+            'Accept-Language: en-US,en;q=0.9',
+            'Referer: https://opendata.jatimprov.go.id/',
+        ],
+        'timeout' => 30
+    ],
+    'ssl' => [
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+    ]
+]);
+
+// Fetch data dengan header
+$jsonData = file_get_contents($apiUrl, false, $context);
+
+// cek jika data berhasil diambil
+if ($jsonData === false) {
+    die('Failed to fetch data from API');
+}
+
+// ubah JSON menjadi array
+$arrayData = json_decode($jsonData, true);
+// cek jika JSON berhasil di-decode
+if ($arrayData === null) {
+    die('Failed to decode JSON data');
+}
+
+// ekstrak data yang diinginkan dari array
+
+$result = [
+    'data' => $arrayData['results'] ?? $arrayData, 
+];
+
+// tampilkan data dalam format JSON
+header('Content-Type: application/json');
+echo json_encode($result['data']['data'], JSON_PRETTY_PRINT);
+
+?>
